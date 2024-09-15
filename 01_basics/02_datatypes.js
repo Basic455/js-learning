4b80975e962f67e22fa4f6d4f1495fe630302128
SUMIT SINGH <110274267+Basic455@users.noreply.github.com>
2024-09-15 16:29:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") we are using node js, not browser
+
+let name = "sumit"
+let age = 26
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
